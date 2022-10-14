f4ea064b3bad10d706f587c52275243532573107
Felipe <feliperara2000@hotmail.com>
2022-10-13 23:46:18 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private router: Router,
+  ) {}
+
+  entrar(){
+    this.router.navigateByUrl('login');
+  }
+  register(){
+    this.router.navigateByUrl('register');
+  }
 
 }
---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+})
+export class TabsPage {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
